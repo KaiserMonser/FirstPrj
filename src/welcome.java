00708f6402d4476ce1a9b5340970ebe5716406f8
KaiserMonster <phucantomicpro@gmai.com>
2024-08-15 09:27:11 +0700

    First Commit

---
+++ src/welcome.java
@@ -0,0 +1,6 @@
+public class welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Hong Phuc !");
+        System.out.println("Welcome to the DHHTTT19A!");
+    }
+}
